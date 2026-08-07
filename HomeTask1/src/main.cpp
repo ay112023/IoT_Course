5eafa845c0724ddf433853356b9a53ffa9e750fe
ay112023 <ay1@ukr.net>
2026-08-07 18:35:49 +0300

    A bit of refactoring

--- HomeTask1/src/main.cpp
+++ HomeTask1/src/main.cpp
@@ -3,43 +3,56 @@
 #define LED 2
 #define EXT_LED 4
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED, OUTPUT);
-  pinMode(EXT_LED, OUTPUT);
+#define DASH_DELAY 600
+#define DOT_DELAY  200
+#define PAUSE_DELAY 100
+#define DIVIDER_DELAY 500
+
+void dash(){
+    digitalWrite(LED, HIGH);
+    digitalWrite(EXT_LED, HIGH);
+    Serial.print("-");
+    delay(DASH_DELAY);
+    digitalWrite(LED, LOW);
+    digitalWrite(EXT_LED, LOW);
+    delay(PAUSE_DELAY);
+}
+
+void dot(){
+    digitalWrite(LED, HIGH);
+    digitalWrite(EXT_LED, HIGH);
+    Serial.print(".");
+    delay(DOT_DELAY);
+    digitalWrite(LED, LOW);
+    digitalWrite(EXT_LED, LOW);
+    delay(PAUSE_DELAY);
 }
 
 void sendS(){
  for(int i = 0; i < 3; i++)
  { 
-    digitalWrite(LED, HIGH);
-    digitalWrite(EXT_LED, HIGH);
-    Serial.print("."); 
-    delay(200);
-    digitalWrite(LED, LOW);
-    digitalWrite(EXT_LED, LOW);
-    delay(100);
-  }
-  delay(100);
+   dot();
+ }
+ delay(PAUSE_DELAY);
 }
 
 void sendO(){
  for(int i = 0; i < 3; i++)
  { 
-    digitalWrite(LED, HIGH);
-    digitalWrite(EXT_LED, HIGH);
-    Serial.print("-");
-    delay(600);
-    digitalWrite(LED, LOW);
-    digitalWrite(EXT_LED, LOW);
-    delay(100);
-  }
-  delay(100);
+   dash();
+ }
+ delay(PAUSE_DELAY);
 }
 
 void sendDivider(){
   Serial.println(); 
-  delay(500);  
+  delay(DIVIDER_DELAY);  
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  pinMode(EXT_LED, OUTPUT);
 }
 
 void loop() {
