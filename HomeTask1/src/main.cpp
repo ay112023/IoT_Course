11e25841dee15ddb26ef64963b3589417c7b6111
ay112023 <ay1@ukr.net>
2026-08-07 19:58:54 +0300

    dwer A bit of refactoring

--- HomeTask1/src/main.cpp
+++ HomeTask1/src/main.cpp
@@ -3,29 +3,31 @@
 #define LED 2
 #define EXT_LED 4
 
+#define DASH "-"
+#define DOT "."
+
 #define DASH_DELAY 600
 #define DOT_DELAY  200
 #define PAUSE_DELAY 100
 #define DIVIDER_DELAY 500
 
-void dash(){
+void sym(int sym_delay, int pause_delay, String symbol)
+{
     digitalWrite(LED, HIGH);
     digitalWrite(EXT_LED, HIGH);
-    Serial.print("-");
-    delay(DASH_DELAY);
+    Serial.print(symbol);
+    delay(sym_delay);
     digitalWrite(LED, LOW);
     digitalWrite(EXT_LED, LOW);
-    delay(PAUSE_DELAY);
+    delay(pause_delay);
+}
+
+void dash(){   
+    sym(DASH_DELAY, PAUSE_DELAY, DASH);
 }
 
 void dot(){
-    digitalWrite(LED, HIGH);
-    digitalWrite(EXT_LED, HIGH);
-    Serial.print(".");
-    delay(DOT_DELAY);
-    digitalWrite(LED, LOW);
-    digitalWrite(EXT_LED, LOW);
-    delay(PAUSE_DELAY);
+    sym(DOT_DELAY, PAUSE_DELAY, DOT);
 }
 
 void sendS(){
